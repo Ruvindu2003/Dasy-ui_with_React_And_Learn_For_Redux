9ca508e31c4a0c7a86ada3402f6c3c46e0b1741f
Ruvindu <ruvindusharada22@gmail.com>
2025-11-27 23:53:45 +0530

    Add routes for editing students

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Contact from './component/Contact'
 import Home from './component/Home'
-import Dashboard from './component/Dashboard'
+
 import StudentList from './component/StudentList'
 import AddStudent from './component/AddStudent'
 import UserList from './component/UserList'
 import AddUser from './component/AddUser'
+import DashBoard from './component/DashBoard'
 
 
 function App() {
@@ -20,9 +21,10 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/dashboard' element={<DashBoard />} />
         <Route path='/studentList' element={<StudentList />} />
         <Route path='/addStudent' element={<AddStudent />} />
+        <Route path='/editStudent/:id' element={<AddStudent />} />
 
         {/* User Management Routes */}
         <Route path='/userList' element={<UserList />} />
