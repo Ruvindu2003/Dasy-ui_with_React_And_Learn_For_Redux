eac44f8b23e32b9e2ea7cc560b83538f860ecd7c
Ruvindu <ruvindusharada22@gmail.com>
2026-01-21 21:52:40 +0530

    fix nav

--- src/component/Manufile.jsx
+++ src/component/Manufile.jsx
@@ -5,11 +5,11 @@ import { NavLink } from "react-router-dom"
 
 
 
-const ManuFile = ({ linkname, path }) => {
+const ManuFile = (props) => {
 
 
     return (
-        <NavLink to={path} className={({ isActive }) => `btn btn-ghost normal-case text-sm ${isActive ? 'bg-base-200 text-primary' : ''}`}>{linkname}</NavLink>
+        <NavLink to={props.path}  className={({ isActive }) => `btn btn-ghost normal-case text-sm ${isActive ? 'bg-base-200 text-primary' : ''}`}>{props.linkname}</NavLink>
     )
 
 }
